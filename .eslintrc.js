3d516412a91ab7de0f3d662f7b43dbf3efdf345c
Nick Makes <nick.makes@hawks.com>
2023-08-11 10:15:29 -0400

    fix: ci script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
-    extends: ['airbnb-typescript/base'],
-    parserOptions: {
-        project: './tsconfig.json'
-    }
-  };
+  extends: ['airbnb-typescript/base', 'plugin:import/recommended'],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  rules: {
+    'import/namespace': 'warn',
+    'import/named': 'warn',
+    '@typescript-eslint/comma-dangle': 'warn',
+    '@typescript-eslint/no-shadow': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-use-before-define': 'warn'
+  }
+};
